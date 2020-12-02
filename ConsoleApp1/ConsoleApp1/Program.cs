857e263de63971d633e473a27ed2cb82b77dd070
SANA BEN <sana.bengannoune@gmail.com>
2020-12-02 13:03:19 +0100

    les variables done

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // ::::::::::::::les variables:::::::::::::::::::::::::::::::::::::::::::::::::
+            /*Console.WriteLine("écrivez le Nom de produit : ");
+            string produit = Console.ReadLine(); // nom de produit
+            Console.WriteLine("écrivez le prix de " + produit + ":");
+            int ht = int.Parse(Console.ReadLine()); // prix hors tax
+            float tva = 0.19f;
+            float ttc = ht * (tva + 1); // prix ttc
+            Console.WriteLine("le prix TTC de " + produit + " est de " + (int)ttc + " DH");
+            */
+            // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+            // ::::::::::::::les boocles:::::::::::::::::::::::::::::::::::::::::::::::::
+        }
+    }
+}
